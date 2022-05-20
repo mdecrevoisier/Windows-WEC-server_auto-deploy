d1a42fd56c41bb84ee03ac927bd74cec72c8bd7a
Michel de CREVOISIER <52909656+mdecrevoisier@users.noreply.github.com>
2022-05-20 09:03:10 +0200

    change file path

---
+++ 1_Set-WEC-role.ps1
@@ -0,0 +1,249 @@
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+### Author : Michel de CREVOISIER
+### v2.60 - 03/09/2021
+###
+### Tested on: Server 2008 R2 (requires removal of <#Requires -RunAsAdministrator> line), Server 2012 R2, Server 2016 and Server 2019
+### Script purpose: configure a Windows host as a Windows collector server
+### Script steps:
+###     1-Enable Windows Event Collector service
+###     2-Enable Windows Remote Management service
+###     3-Prompt for switching (or not) to a custom port and create appropriate firewall rule
+###     4-Fixes ACL permissions on WinRM URLs (https://support.microsoft.com/en-us/help/4494462/events-not-forwarded-if-the-collector-runs-windows-server-2019-or-2016)
+###         4.1-Verify if WEC servers run Server 2016 or higher
+###         4.2-Stop relavant services (NXLog agent, Collector and WinRM)
+###         4.3-Delete and replace ACLs
+###         4.4-Start relevant services (NXLog agent, Windows Collector and WinRM)
+###
+
+
+# Requires PowerShell console to run as an administrator (compatible with PowerShell v4 or higher only)
+#Requires -RunAsAdministrator
+
+
+###### 1-Enable Windows Event Collector Service ######
+write-host ""
+write-host "1-Enabling Windows Event collector service" -foregroundcolor "yellow"
+$wecutil_init = "C:\Windows\System32\wecutil.exe"
+$wecutil_arg = "qc -q"
+
+# Start wecutil with args and reports
+$wecutil_exec = Start-Process $wecutil_init -ArgumentList $wecutil_arg -NoNewWindow -PassThru -Wait
+if ($wecutil_exec.ExitCode -eq 0) {
+    write-host "Windows Event collector service successfully configured" -foregroundcolor "green" 
+}
+else {
+    Write-Host "Windows Event collector configuration failure:" $wecutil_exec.ExitCode  -foregroundcolor "red" 
+    Break
+}
+
+
+
+###### 2-Configure Windows Remote Management service ######
+write-host ""
+write-host "2-Configuring WinRM service" -foregroundcolor "yellow"
+$winrm_init = "C:\Windows\System32\winrm.cmd"
+$winrm_arg = "qc -q"
+
+# Verifies WinRM is set to 'Automatic Startup' or sets it
+if (((Get-Service -Name WinRM).StartType) -ne "Automatic") { Set-Service -Name WinRM -StartupType Automatic }
+
+# Starts WinRM with Args
+$winrm_exec = Start-Process $winrm_init -ArgumentList $winrm_arg -NoNewWindow -PassThru -Wait
+if ($winrm_exec.ExitCode -eq 0) {
+    write-host "WinRM Service service successfully configured" -foregroundcolor "green" 
+}
+else {
+    Write-Host "WinRM Service configuration failure:" $winrm_exec.ExitCode -foregroundcolor "red" 
+    Break
+}
+
+
+
+###### 3-Prompt for switching (or not) to a custom port and create appropriate firewall rule ######
+write-host ""
+write-host "3-Would you like to keep the default port (TCP 5985) or use a custom TCP port for the WinRM listener on the WEC server?" -foregroundcolor "yellow"
+
+# Prompt requesting for choosing the port
+$option_port_custom = Read-Host -Prompt "Changing the default WinRM port may allow to easily identify traffic related to log transport and also to apply traffic shaping rules (QoS). If you choose a customer port, the subscription manager target port defined in the GPO will need to be adjusted. Would you like to use a custom port (y/n)?" 
+
+if ($option_port_custom -eq "y") {
+    write-host ""
+    write-host "CUSTOM port selected" -foregroundcolor "green" 
+
+    # 3.1 Prompting for default port
+    $winrm_port = Read-Host -Prompt "Enter the custom WinRM port (should be higher than > 1024, <65535 and not in use on this server)" 
+
+    # 3.2 Verifying if provided port is valid
+    if (($winrm_port -ge 1025) -and ($winrm_port -le 65535)) {
+        write-host ""
+        write-host "Valid port provided" -foregroundcolor "green" 
+    }
+    else {
+        write-host ""
+        Write-Host "Unvalid port provided, aborting"  -foregroundcolor "red" 
+        Break
+    }
+
+    # 3.3 Changing to custom port
+    $winrm_arg = "winrm set winrm/config/Listener?Address=*+Transport=HTTP '@{Port=""$winrm_port""}'"
+    Invoke-Expression $winrm_arg
+
+    # 3.4 Deleting former firewall rule in case previous config exists
+    $wec_rule_name = "WinRM custom for WEC server"
+    Remove-NetFirewallRule -DisplayName $wec_rule_name -ErrorAction Ignore
+
+    # 3.5 Creating new firewall rule
+    New-NetFirewallRule -DisplayName  $wec_rule_name -Direction Inbound -LocalPort $winrm_port -Protocol TCP -Action Allow | out-null
+    write-host ""
+    write-host "Custom firewall rule for WinRM successfully created" -foregroundcolor "green" 
+}
+elseif ($option_port_custom -eq "n") {
+    write-host ""
+    write-host "Keeping DEFAULT configuration" -foregroundcolor "green" 
+    $winrm_port=5985
+}
+else {
+    write-host ""
+    Write-Host "Unknown option provided, aborting"  -foregroundcolor "red" 
+    Break
+}
+
+
+
+###### 4-Updating SDDL permissions on WinRM listener (if necessary) ######
+write-host ""
+write-host "4.1-Verifying if WEC servers run Server 2016 or higher" -foregroundcolor "yellow"
+
+# Get version
+$os_version = (Get-CimInstance Win32_OperatingSystem).version
+# Server 2012 R2 = version number 6.3
+# Server 2016 = version number v10.x
+# Server 2019 = version number v10.x
+
+if ($os_version -ge 10) {
+    write-host "Windows Server 2016 or higher detected. WinRM permissions need to be rewritten." -foregroundcolor "green" 
+
+    ###### 4.2-Stopping dependant services ######
+
+    write-host ""
+    write-host "4.2-Stopping dependant services" -foregroundcolor "yellow"
+
+    # Stop Event log collector service
+    write-host ""
+    write-host "Stopping Event collector service" -foregroundcolor "Magenta"
+    Stop-Service Wecsvc
+
+    # Ensure that collector service is stopped
+    if (((Get-Service -Name wecsvc).Status) -ne "Stopped") {
+        Write-Host "Collector Service not stopped, aborting"  -foregroundcolor "red" 
+        Break
+    }
+
+    # Stop WinRM service
+    write-host ""
+    write-host "Stopping WinRM service" -foregroundcolor "Magenta"
+    Stop-Service WinRM
+
+    # Ensure that WinRM service is stopped
+    if (((Get-Service -Name WinRM).Status) -ne "Stopped") {
+        Write-Host "Collector Service not stopped, aborting"  -foregroundcolor "red" 
+        Break
+    }
+
+    # Stop NXLog service (if exists)
+    $nxlog_service = "nxlog"
+    if (Get-Service $nxlog_service -ErrorAction SilentlyContinue) {
+        if (((Get-Service -Name $nxlog_service).Status) -ne "Stopped") {
+            write-host "Stopping NXLog service" -foregroundcolor "Magenta"
+            Stop-Service $nxlog_service
+            
+            # Ensure that NXLog service is stopped
+            if (((Get-Service -Name $nxlog_service).Status) -ne "Stopped") {
+                Write-Host "NXLog Service not stopped, aborting"  -foregroundcolor "red" 
+                Break
+            }
+        }
+    }
+
+
+    ###### 4.3-Cleaning and setting proper ACLs for WinRM service ######
+
+    write-host ""
+    write-host "4.3-Cleaning and setting proper ACLs for WinRM service" -foregroundcolor "yellow"
+
+    ## We delete existing URLs for WinRM
+    write-host ""
+    write-host "Deleting existing ACLs for WinRM" -foregroundcolor "Magenta"
+    cmd.exe /c "netsh http delete urlacl url=http://+:$winrm_port/wsman/"
+
+    write-host ""
+    write-host "Setting proper ACLs for WinRM" -foregroundcolor "Magenta"
+    cmd.exe /c "netsh http add urlacl url=http://+:$winrm_port/wsman/ sddl=D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)"
+
+    write-host ""
+    write-host "Checking added ACLs" -foregroundcolor "Magenta"
+    $serviceUrlHttp = "http://+:$winrm_port/wsman/"
+    cmd.exe /c "netsh http show urlacl url=$serviceUrlHttp"
+
+
+    ###### 4.4-Starting dependant services ######
+    write-host ""
+    write-host "4.4-Starting dependant services" -foregroundcolor "yellow"
+
+    ## We start the WEC service 
+    write-host ""
+    write-host "Starting Event collector service" -foregroundcolor "Magenta"
+
+    Start-service Wecsvc
+    if (((Get-Service -Name wecsvc).Status) -ne "Running") {
+        Write-Host "Collector Service not started, aborting"  -foregroundcolor "red" 
+    }
+    else {
+        write-host "Collector service successfully started, continuing..." -foregroundcolor "green" 
+    }
+
+    ## We start the WinRM service 
+    write-host ""
+    write-host "Starting WinRM  service" -foregroundcolor "Magenta"
+
+    Start-service WinRM
+    if (((Get-Service -Name WinRM).Status) -ne "Running") {
+        Write-Host "WinRM service not started, aborting"  -foregroundcolor "red" 
+    }
+    else {
+        write-host "WinRM service successfully started, continuing..." -foregroundcolor "green" 
+    }
+
+    ## We start the NXLog service
+    if (Get-Service $nxlog_service -ErrorAction SilentlyContinue) {
+        write-host ""
+        write-host "Starting NXLog service" -foregroundcolor "Magenta"
+
+        Start-service $nxlog_service
+        if (((Get-Service -Name $nxlog_service).Status) -ne "Running") {
+            Write-Host "NXLog service could not be started. Requires manual start"  -foregroundcolor "red" 
+            write-host ""
+        }
+        else {
+            write-host "NXLog service successfully started, continuing..." -foregroundcolor "green" 
+            write-host ""
+        }
+    }
+}
+else {
+    Write-Host "Windows Server 2012 R2 or lower detected, no permissions changes required."  -foregroundcolor "yellow" 
+}
+
+
+
+###### FINAL CONFIRMATION ######
+write-host ""
+write-host "#########################################" -foregroundcolor "green" 
+write-host " WEC server role successfully configured" -foregroundcolor "green" 
+write-host "#########################################" -foregroundcolor "green" 
+write-host ""
---
+++ 2_Deploy-Toolkit.ps1
@@ -0,0 +1,305 @@
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+### Author : Michel de CREVOISIER
+### v2.60 - 03/09/2021
+###
+### Tested on: Server 2008 R2 (requires removal of <#Requires -RunAsAdministrator>), Server 2012 R2, Server 2016 and Server 2019
+### Script purpose: import custom channels and subscriptions to enhance Windows log collection
+### Script steps:
+###     1-Verify if configuration folders are present
+###     2-Prompt for moving (or not) custom event logs to a separated hard drive
+###     3-Stop dependant Windows services (NXLog and WEC)
+###     4-Import custom channel structure
+###     5-Move event log files to a separated hard drive (if requested)
+###     6-Increase default log files size
+###     7-Start relevant services (NXLog and Windows Collector)
+###     8-Import custom subscriptions
+###
+
+
+# Requires PowerShell console to run as an administrator (compatible with PowerShell v4 or higher only)
+#Requires -RunAsAdministrator
+
+
+###### 1-Verify if configuration folders are present ######
+write-host ""
+write-host "1-Verifying configuration folders presence" -foregroundcolor "yellow"
+if ((Test-Path "windows-event-channels") -And (Test-Path "windows-subscriptions"){
+    write-host "Configuration folders exist, continuing..." -foregroundcolor "green" 
+}
+else {
+    Write-Host "Configuration folder(s) missing, aborting"  -foregroundcolor "red" 
+    Break
+}
+
+
+###### 2-Prompt for moving (or not) custom event logs to a separated hard drive ######
+write-host ""
+write-host "3-Moving custom event log files to a separated hard drive?" -foregroundcolor "yellow"
+
+# Prompt requesting for moving custom (or not) event log to a separated hard drive
+$Option_custom_location = Read-Host -Prompt "It is recommended to move the new custom event log files to a separated hard drive. Would like to proceed with this recommended approach (y/n)?" 
+
+if ($Option_custom_location -eq "y") {
+    $LogFolderPath = Read-Host -Prompt "Enter the folder path located on a separated hard drive" 
+
+    # if $LogFolderPath does not end with a "\", we had a it
+    if ($LogFolderPath -notmatch '.+?\\$'){
+        $LogFolderPath += '\'
+        write-host $LogFolderPath
+    }
+
+    write-host "Verifying path..." -foregroundcolor "yellow" 
+    if ((Test-Path $LogFolderPath) -And (Test-Path $LogFolderPath -PathType Container)){
+        write-host "Path is valid, continuing..." -foregroundcolor "green" 
+    }
+    else {
+        Write-Host "Path is not valid or does not exist, aborting"  -foregroundcolor "red" 
+        Break
+    }
+}
+else {
+    write-host "Keeping custom event log files in the default location C:\Windows\System32\winevt\Logs\" -foregroundcolor "magenta" 
+}
+
+
+
+###### 3-Prompt for choosing standard or compact toolkit mode ######
+write-host ""
+write-host "2-Would you like to go with 'standard' or 'compact' toolkit mode?" -foregroundcolor "yellow"
+
+# Prompt requesting for choosing the toolkit mode
+$Option_toolkit_mode = Read-Host -Prompt "Standard mode is the default and most suitable mode for most organization with less than 500 source clients. 
+In case of having more than 500 source clients reporting to this WEC collector, choose the compact mode. More informations in documentation, point 3.10.
+Would like to go with 'standard'(s) or 'compact' (c) mode deployement (s/c)?" 
+
+if ($Option_toolkit_mode -eq "s") {
+    write-host "STANDARD mode selected" -foregroundcolor "green" 
+    $Subscription_folder_path = "..\wef-subscriptions_STANDARD" 
+}
+elseif ($Option_toolkit_mode -eq "c") {
+    write-host "COMPACT mode selected" -foregroundcolor "green" 
+    $Subscription_folder_path = "..\wef-subscriptions_COMPACT" 
+}
+else {
+    Write-Host "Unknown option provided, aborting"  -foregroundcolor "red" 
+    Break
+}
+
+
+
+###### 4-Stopping dependant services ######
+# In case there's a third party agent for collecting logs, it shall be stopped. Here NXLog is used as an example. 
+
+write-host ""
+write-host "4-Stopping dependant services" -foregroundcolor "yellow"
+
+# Stop Event log collector service
+write-host ""
+write-host "Stopping Event collector service" -foregroundcolor "Magenta"
+Stop-Service Wecsvc
+
+# Ensure that collector service is stopped
+if (((Get-Service -Name wecsvc).Status) -ne "Stopped") {
+    Write-Host "Collector Service not stopped, aborting"  -foregroundcolor "red" 
+    Break
+}
+
+# Stop NXLog service (if exists)
+$nxlog_service = "nxlog"
+if (Get-Service $nxlog_service -ErrorAction SilentlyContinue) {
+    if (((Get-Service -Name $nxlog_service).Status) -ne "Stopped") {
+        write-host "Stopping NXLog service" -foregroundcolor "Magenta"
+        Stop-Service $nxlog_service
+        
+        # Ensure that NXLog service is stopped
+        if (((Get-Service -Name $nxlog_service).Status) -ne "Stopped") {
+            Write-Host "NXLog Service not stopped, aborting"  -foregroundcolor "red" 
+            Break
+        }
+    }
+}
+
+
+
+###### 5-Import custom channel structure ######
+write-host ""
+write-host "5-Importing custom channel structure" -foregroundcolor "yellow"
+
+
+# Unload the current Event Channel structure (if existing)
+if (Test-Path C:\windows\system32\CustomEventChannels.man){
+    write-host ""
+    write-host "Unloading default channel structure" -foregroundcolor "Magenta"
+    wevtutil uninstall-manifest C:\windows\system32\CustomEventChannels.man
+    write-host "Successfully unloaded channel structure" -foregroundcolor "green" 
+}
+
+# Overwrite channel structure files
+write-host ""
+write-host "Copying custom channel structure files" -foregroundcolor "Magenta"
+Set-Location "windows-event-channels"
+if ((Test-Path CustomEventChannels.dll) -And (Test-Path CustomEventChannels.man)){
+    Copy-Item CustomEventChannels.dll C:\Windows\system32\
+    Copy-Item CustomEventChannels.man C:\Windows\system32\
+}
+else {
+    Write-Host "Source files missing, aborting"  -foregroundcolor "red" 
+    Break
+}
+
+# Load the new Event channel structure
+write-host ""
+write-host "Loading custom channel structure" -foregroundcolor "Magenta"
+wevtutil im C:\windows\system32\CustomEventChannels.man
+write-host "Successfully loaded channel structure" -foregroundcolor "green" 
+
+
+###### 6-Move all event log files to a separated hard drive ######
+
+$EventLogs = wevtutil enum-logs | select-string -pattern "WEC"
+$wevtutil_init = "C:\Windows\System32\wevtutil.exe"
+
+if ($Option_custom_location -eq "y") {
+
+    write-host ""
+    write-host "4-Moving custom event log files to path: $LogFolderPath"  -foregroundcolor "yellow"
+
+    foreach($EventLog in $EventLogs)
+    {
+        $full_path = $LogFolderPath + $EventLog + ".evtx"
+        $wevtutil_arg_move = "sl $EventLog /lfn:$full_path"
+        $wevtutil_exec_move = Start-Process $wevtutil_init -ArgumentList $wevtutil_arg_move -NoNewWindow -PassThru -Wait
+        if ($wevtutil_exec_move.ExitCode -eq 0) {
+                write-host "$EventLog event log successfully moved" -foregroundcolor "green" 
+            }
+            else {
+                Write-Host "WEVTUTIL Exitcode:" $wevtutil_exec_move.ExitCode  -foregroundcolor "red" 
+                }
+    }
+}
+
+
+###### 7-Increasing custom event log file size ######
+write-host ""
+write-host "7-Increasing custom event log file size" -foregroundcolor "yellow"
+
+# 6.1 Increase all custom event log file size to 128MB
+write-host ""
+write-host "   7.1-Increasing all log files size to 128 MB" -foregroundcolor "yellow"
+foreach($EventLog in $EventLogs)
+{
+    $log_size_default = 134217728 # 128MB defined in KB
+    $wevtutil_arg_size = "sl $EventLog /ms:$log_size_default"
+    $wevtutil_exec_size = Start-Process $wevtutil_init -ArgumentList $wevtutil_arg_size -NoNewWindow -PassThru -Wait
+    if ($wevtutil_exec_size.ExitCode -eq 0) {
+            write-host "$EventLog event log size was successfully increased to $log_size_default Bytes" -foregroundcolor "green" 
+        }
+        else {
+            Write-Host "WEVTUTIL failed to resied the event log:" $wevtutil_exec_size.ExitCode  -foregroundcolor "red" 
+            }
+}
+
+# 6.2-Increasing high usage log files to higher size
+write-host ""
+write-host "   7.2-Increasing high usage log files to higher size" -foregroundcolor "yellow"
+
+# Verify CSV path
+write-host ""
+write-host "Verifying CSV file..." -foregroundcolor "yellow" 
+$CSV_file = "custom_log_size_settings.csv"
+    if ((Test-Path $CSV_file) -And (Test-Path $CSV_file -PathType leaf)){
+        write-host "CSV file is valid, continuing..." -foregroundcolor "green" 
+        write-host ""
+    }
+    else {
+        Write-Host "CSV file is not valid or does not exist, aborting"  -foregroundcolor "red" 
+        Break
+    }
+
+# Import CSV file and increase size
+$Imported_CSV_file = Import-Csv $CSV_file
+ForEach ($item in $Imported_CSV_file){ 
+    $EventLog = $($item.Log_name)
+    $High_Log_Size = $($item.Log_size_in_Bytes)
+
+    # Increase size
+    $wevtutil_arg_size = "sl $EventLog /ms:$High_Log_Size"
+    $wevtutil_exec_size = Start-Process $wevtutil_init -ArgumentList $wevtutil_arg_size -NoNewWindow -PassThru -Wait
+    if ($wevtutil_exec_size.ExitCode -eq 0) {
+            write-host "$EventLog event log size was successfully increased to $High_Log_Size Bytes" -foregroundcolor "green" 
+        }
+        else {
+            Write-Host "WEVTUTIL failed to resied the event log:" $wevtutil_exec_size.ExitCode  -foregroundcolor "red" 
+            }
+
+
+}
+
+
+###### 8-Starting dependant services ######
+write-host ""
+write-host "8-Starting dependant services" -foregroundcolor "yellow"
+
+## We start the WEC service before importing the subscriptions
+write-host ""
+write-host "Starting Event collector service" -foregroundcolor "Magenta"
+
+Start-service Wecsvc
+if (((Get-Service -Name wecsvc).Status) -ne "Running") {
+    Write-Host "Collector Service not started, aborting"  -foregroundcolor "red" 
+}
+else {
+    write-host "Collector service successfully started, continuing..." -foregroundcolor "green" 
+}
+
+## We start the NXLog service
+if (Get-Service $nxlog_service -ErrorAction SilentlyContinue) {
+    write-host ""
+    write-host "Starting NXLog service" -foregroundcolor "Magenta"
+
+    Start-service $nxlog_service
+    if (((Get-Service -Name $nxlog_service).Status) -ne "Running") {
+        Write-Host "NXLog service could not be started. Requires manual start"  -foregroundcolor "red" 
+        write-host ""
+    }
+    else {
+        write-host "NXLog service successfully started, continuing..." -foregroundcolor "green" 
+        write-host ""
+    }
+}
+
+
+###### 9-Importing WEC subscriptions ######
+write-host ""
+write-host "9-Importing WEC subscriptions" -foregroundcolor "yellow"
+
+#Import all XML subscriptions
+Set-Location $Subscription_folder_path
+$wecutil_init = "C:\Windows\System32\wecutil.exe"
+
+ForEach ($Subscription in Get-ChildItem *.xml)
+{
+    write-host "Attempting to import subscription: $Subscription" -foregroundcolor "yellow"
+    $wecutil_arg2 = "create-subscription $Subscription"
+    $wecutil_exec2 = Start-Process $wecutil_init -ArgumentList $wecutil_arg2 -NoNewWindow -PassThru -Wait
+    if ($wecutil_exec2.ExitCode -eq 0) {
+        write-host "$Subscription subscription successfully imported" -foregroundcolor "green" 
+    }
+    else {
+        Write-Host "WECUTIL Exitcode:" $wecutil_exec2.ExitCode  -foregroundcolor "red" 
+        }
+}
+
+
+
+###### FINAL CONFIRMATION ######
+write-host ""
+write-host "################################################" -foregroundcolor "green" 
+write-host " WEC toolkit deployment successfully performed"  -foregroundcolor "green" 
+write-host "################################################" -foregroundcolor "green" 
+write-host ""
